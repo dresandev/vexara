d3f9bc14a4dc2dc77f4f1771cf3ca892adec3804
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-04-01 20:53:39 -0500

    add: warning that the payment may fail

--- src/app/payment-credentials/page.tsx
+++ src/app/payment-credentials/page.tsx
@@ -4,6 +4,7 @@ export default function PaymentCredentialsPage() {
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Mercado pago credenciales de prueba</h1>
+      <p className={styles.description}>En ocasiones el pago es rechazado por x razón y no depende de la app, solo inténtalo más tarde.</p>
       <p>Usuario: TESTUSER1292447665</p>
       <p>Contraseña: qLv6ZM0LLb</p>
       <a
