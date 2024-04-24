53c9c97d303c6d6cce8548474b4bdaba5c6cae8c
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-04-24 01:18:40 -0500

    add: vexara test credentials

--- src/app/payment-credentials/page.tsx
+++ src/app/payment-credentials/page.tsx
@@ -4,9 +4,11 @@ export default function PaymentCredentialsPage() {
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Mercado pago credenciales de prueba</h1>
-      <p className={styles.description}>En ocasiones el pago es rechazado por x razón y no depende de la app, solo inténtalo más tarde.</p>
-      <p>Usuario: TESTUSER1292447665</p>
-      <p>Contraseña: qLv6ZM0LLb</p>
+      <p className={styles.description}>En ocasiones el pago es rechazado por razones que desconozco (cabe resaltar que sucede en modo desarrollo/test) y no depende de la app, puedes intentarlo más tarde.</p>
+      <p>Usuario vexara: test@test.com</p>
+      <p>Contraseña vexara: test123TEST</p>
+      <p>Usuario mercadopago: TESTUSER1292447665</p>
+      <p>Contraseña mercadopago: qLv6ZM0LLb</p>
       <a
         className={styles.link}
         href='https://www.mercadopago.com.co/developers/es/docs/checkout-api/additional-content/your-integrations/test/cards'
